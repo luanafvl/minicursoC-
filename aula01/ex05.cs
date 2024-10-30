19ccf62ae2620b301e4032efea7e14ad2f2b9b8e
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2024-10-30 19:06:55 -0300

    Create ex05.cs

---
+++ aula01/ex05.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("A");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nB");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nC");
+int C = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nD");
+int D = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Diferenca = {A * B - C * D}");
